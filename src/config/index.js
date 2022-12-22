ace2e7be478e2355352cb5c17919d8ee206b14ba
Yasser Loukniti <yasser.loukniti@estiam.com>
2022-12-22 10:20:12 +0100

    Install template

---
+++ src/config/index.js
@@ -0,0 +1,7 @@
+// or get from process.env.REACT_APP_{var} to handle PROD and DEV environments
+export const APP_VERSION = '1.0.0';
+export const API_BASE_URL = 'https://jsonplaceholder.typicode.com';
+export const API_BASE_URL_SEC = 'http://localhost:3001';
+export const ENABLE_REDUX_LOGGER = false;
+
+export default {};
