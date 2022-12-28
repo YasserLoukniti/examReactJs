bc2efb192f59df02178beff686287ebb95152f93
YL <yass_official@outlook.fr>
2022-12-28 21:31:58 +0100

    CRUD Profile

--- src/services/profilesService.js
+++ src/services/profilesService.js
@@ -17,6 +17,62 @@ class ProfilesService {
         reject(err);
       });
   });
+  updateProfile = (profile) => new Promise((resolve, reject) => {
+    axios({
+      method: 'PUT',
+      url: `${API_BASE_URL}/v1/users/${profile.id}`,
+      headers: {
+        'x-access-token': localStorage.getItem('accessToken')
+      },
+      body:{
+        usernname:profile.username,
+        email:profile.email,
+        phone:profile.phone
+      }
+    })
+      .then((res) => {
+        resolve(res.data);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+  deleteProfile = (profileID) => new Promise((resolve, reject) => {
+    axios({
+      method: 'DELETE',
+      url: `${API_BASE_URL}/v1/users/${profileID}`,
+      headers: {
+        'x-access-token': localStorage.getItem('accessToken')
+      }
+    })
+      .then((res) => {
+        resolve(res.data);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+  createProfile = (profile) => new Promise((resolve, reject) => {
+    axios({
+      method: 'POST',
+      url: `${API_BASE_URL}/v1/users`,
+      headers: {
+        'x-access-token': localStorage.getItem('accessToken')
+      },
+      body:{
+        firstName: profile.firstName,
+        lastName: profile.lastName,
+        email: profile.email,
+        password: profile.password
+      }
+    })
+      .then((res) => {
+        resolve(res.data);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
 }
 
 const profilesService = new ProfilesService();
